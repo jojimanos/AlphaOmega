f1cf0c27d41577d5ad00e931074a54a9f2e967ba
jojimanos <jojimanos@github.com>
2022-07-22 02:34:19 +0300

    Dropdown design

--- pages/components/dropdown.jsx
+++ pages/components/dropdown.jsx
@@ -20,19 +20,10 @@ class Dropdown extends React.Component {
               <p className="dropdown" onClick={this.toggleOpen}>
                 <button className="
           dropdown-toggle
-          px-6
-          py-2.5
-          bg-blue-600
-          text-white
-          font-medium
-          text-xs
           leading-tight
-          uppercase
           rounded
-          shadow-md
-          hover:bg-blue-700 hover:shadow-lg
-          focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-          active:bg-blue-800 active:shadow-lg active:text-white
+          hover:bg-gray-200
+          focus:bg-gray-300
           transition
           duration-150
           ease-in-out

b12cfc87709f922509a061b636f3b682c3082f6b
jojimanos <jojimanos@github.com>
2022-07-22 16:08:44 +0300

    Popup connects to google

--- pages/components/popup2.jsx
+++ pages/components/popup2.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Popup from 'reactjs-popup';
+import Link from 'next/link';
+import Script from 'next/script';
 
 export default () => ( <Popup trigger={<button className="button"> Open Modal 
 </button>} modal nested > 
@@ -8,26 +10,15 @@ export default () => ( <Popup trigger={<button className="button"> Open Modal
 </button> 
 <div className="header"> Modal Title 
 </div> 
-<div className="content"> {' '} Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-Atque, a nostrum. Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates 
-delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos? 
-<br /> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit 
-commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae explicabo nemo 
-nam libero ad, doloribus, voluptas rem alias. Vitae? 
+<div className="content">
+<form method="get" action="https://www.google.com/search?q=perseus">
+      <input type="search" id="query" name="q" placeholder="Search..."/>
+      <button>
+        Search
+      </button>
+    </form>
 </div> 
-<div className="actions"> 
-<Popup trigger={<button className="button"> Trigger 
-</button>} 
-position="top center" nested > 
-<span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-Beatae magni omnis delectus nemo, maxime molestiae dolorem numquam mollitia, voluptate ea, 
-accusamus excepturi deleniti ratione sapiente! Laudantium, aperiam doloribus. Odit, aut. 
-</span> 
-</Popup> 
-<button className="button" onClick={() => { console.log('modal closed '); close(); }} > 
-close modal 
-</button> 
-</div> 
+ 
 </div> )}
 </Popup>);
 

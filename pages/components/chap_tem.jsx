7c463388beb8003afcc6659e060962c0affc22a5
jojimanos <jojimanos@github.com>
2022-08-03 02:04:02 +0300

    Additional Chapter

--- pages/components/chap_tem.jsx
+++ pages/components/chap_tem.jsx
@@ -4,13 +4,14 @@ function Chapmath_tem (props) {
     
     const {chapsets} = props
 
-    const {title_1, link_1} = chapsets
+    const {title_1, title_2, link_1} = chapsets
         
     return (
             <div className='text-center m-3 font-serif'>
                 <Link href={link_1}>
                     <div>
-                    <p className='text-3xl'>1 {title_1}</p>
+                    <p className='text-3xl whitespace-pre'>1 {title_1}</p>
+                    <p className='text-3xl whitespace-pre'>2 {title_2}</p>
                     </div>                
                 </Link>
             </div>

87b4bc5bde31ca6296a2cb619158e3e3fc51a518
jojimanos <manosgeorge7@gmail.com>
2022-11-17 22:04:10 +0200

    jsx to string replaced

--- pages/components/htmlToString.jsx
+++ pages/components/htmlToString.jsx
@@ -1,6 +1,6 @@
-import jsxToString from "jsx-to-string";
+import reactElementToJSXString from 'react-element-to-jsx-string';
 
 export default function htmlToString(props) {
     let text
-    return (text = jsxToString(props))
+    return (text = reactElementToJSXString(props))
 }

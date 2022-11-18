0bc08aa0780987537b4e265e78a454a8759619fb
jojimanos <manosgeorge7@gmail.com>
2022-11-18 23:28:18 +0200

    Types

--- pages/components/pageTemplates/gospelListTemplate.tsx
+++ pages/components/pageTemplates/gospelListTemplate.tsx
@@ -8,28 +8,28 @@ function Gosp_tem(props: {gospsets: any}): JSX.Element {
   return (
     <div className="font-serif">
       <div className="text-3xl text-center m-3">
-        <Link href={link_1 as string || ''}>
+        <Link href={link_1 || ''}>
           <div className="p-5">
             <p className="p-2">1 {title_1 as string}</p>
           </div>
         </Link>
       </div>
       <div className="text-3xl text-center m-3">
-        <Link href={link_2 as string || ''}>
+        <Link href={link_2 || ''}>
           <div className="p-5">
             <p className="p-2">2 {title_2 as string}</p>
           </div>
         </Link>
       </div>
       <div className="text-3xl text-center m-3">
-        <Link href={link_3 as string || ''}>
+        <Link href={link_3 || ''}>
           <div className="p-5">
             <p className="p-2">3 {title_3 as string}</p>
           </div>
         </Link>
       </div>
       <div className="text-3xl text-center m-3">
-        <Link href={link_4 as string || ''}>
+        <Link href={link_4 || ''}>
           <div className="p-5">
             <p className="p-2">4 {title_4 as string}</p>
           </div>

451ce6b556546ceee7edff73e75ff086f4bd6368
jojimanos <manosgeorge7@gmail.com>
2022-11-18 19:13:00 +0200

    htmlText and htmlTextEnglish tsx

---
+++ pages/components/htmlText.tsx
@@ -0,0 +1,10 @@
+export default function htmlText({ newtest }: any, props: any) {
+
+    const { author, chapter, paragraph } = props
+
+    const text = <p>{newtest.map((x: any) => (<li><h2>{x[author][chapter][paragraph]}</h2></li>))}</p>
+
+    return (
+        text
+    )
+}
--- pages/components/htmlToString.tsx
+++ pages/components/htmlToString.tsx
@@ -1,6 +1,6 @@
 import reactElementToJSXString from 'react-element-to-jsx-string';
 
-export default function htmlToString(props) {
+export default function htmlToString(props: JSX.Element) {
     let text
     return (text = reactElementToJSXString(props))
 }

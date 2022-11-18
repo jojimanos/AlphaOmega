f2bb9db7a1f4655bd5ed47298562d5e6908eab7b
jojimanos <manosgeorge7@gmail.com>
2022-11-18 20:10:00 +0200

    Types

--- pages/components/htmlEnglishText.tsx
+++ pages/components/htmlEnglishText.tsx
@@ -2,7 +2,7 @@ export default function htmlEnglishText({ newtest }:any, props: any) {
 
     const { authorEnglish, chapter, paragraph } = props
 
-    const text = <p>{newtest.map((x: any) => (<li><h2>{x?.[authorEnglish]?.[chapter]?.[paragraph]}</h2></li>))}</p>
+    const text = <p>{newtest?.map((x: any) => (<li><h2>{x[authorEnglish][chapter][paragraph]}</h2></li>))}</p>
 
     return (
         text
--- pages/components/htmlText.tsx
+++ pages/components/htmlText.tsx
@@ -2,7 +2,7 @@ export default function htmlText({ newtest }: any, props: any) {
 
     const { author, chapter, paragraph } = props
 
-    const text = <p>{newtest.map((x: any) => (<li><h2>{x?.[author]?.[chapter]?.[paragraph]}</h2></li>))}</p>
+    const text = <p>{newtest?.map((x: any) => (<li><h2>{x[author][chapter][paragraph]}</h2></li>))}</p>
 
     return (
         text

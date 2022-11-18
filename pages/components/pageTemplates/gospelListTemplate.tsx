6708513f9a78118135b2356c21a4b3f0274c818c
jojimanos <manosgeorge7@gmail.com>
2022-11-18 22:31:21 +0200

    Types

--- pages/components/pageTemplates/gospelListTemplate.tsx
+++ pages/components/pageTemplates/gospelListTemplate.tsx
@@ -3,13 +3,12 @@ import Link from "next/link";
 function Gosp_tem(props: {gospsets: any}): JSX.Element {
   const { gospsets } = props;
 
-  const { title_1, title_2, title_3, title_4, link_1, link_2, link_3, link_4 } =
-    gospsets;
+  const { title_1, title_2, title_3, title_4, link_1, link_2, link_3, link_4 } = undefined || gospsets;
 
   return (
     <div className="font-serif">
       <div className="text-3xl text-center m-3">
-        <Link href={gospsets?.[link_1 as string]}>
+        <Link href={gospsets[link_1 as string]}>
           <div className="p-5">
             <p className="p-2">1 {gospsets?.[title_1 as string]}</p>
           </div>

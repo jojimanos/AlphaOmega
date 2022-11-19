299b51ecdc4bb0c8aefdb58f1e9f639ee6231c35
jojimanos <manosgeorge7@gmail.com>
2022-11-19 22:37:17 +0200

    fix undefined error on htmlText and htmlEnglishText

--- pages/components/htmlEnglishText.tsx
+++ pages/components/htmlEnglishText.tsx
@@ -2,7 +2,7 @@ export default function htmlEnglishText({ newtest }:any, props: any) {
 
     const { authorEnglish, chapter, paragraph } = props || {};
 
-    const text = <div>{newtest.map((x: any, i: any) => (<h2 key={i}>{x[authorEnglish][chapter][paragraph]}</h2>))}</div>
+    const text = <div>{newtest?.map((x: any, i: any) => (<h2 key={i}>{x[authorEnglish][chapter][paragraph]}</h2>))}</div>
 
     return (
         text
--- pages/components/htmlText.tsx
+++ pages/components/htmlText.tsx
@@ -2,7 +2,7 @@ export default function htmlText({ newtest }: any, props: any) {
 
     const { author, chapter, paragraph } = props || {};
 
-    const text = <div>{newtest.map((x: any, i: any) => (<h2 key={i}>{x[author][chapter][paragraph]}</h2>))}</div>
+    const text = <div>{newtest?.map((x: any, i: any) => (<h2 key={i}>{x[author][chapter][paragraph]}</h2>))}</div>
 
     return (
         text

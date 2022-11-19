6b25a343f85d698f9f1d6b9e940d28c06d14799c
jojimanos <manosgeorge7@gmail.com>
2022-11-19 10:03:59 +0200

    htmlToString deleted

--- pages/components/htmlToString.tsx
+++
@@ -1,8 +0,0 @@
-import reactElementToJSXString from 'react-element-to-jsx-string';
-
-function htmlToString(props: JSX.Element): string {
-    let text
-    return (text = reactElementToJSXString(props))
-}
-
-export default htmlToString

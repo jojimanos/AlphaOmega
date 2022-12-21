afa479cd1ae9faf9d0a34796a601e3b96a73b8fe
jojimanos <manosgeorge7@gmail.com>
2022-12-21 18:52:42 +0200

    Fixed the cannot read properties of undefined error in HtmlText

--- pages/components/htmlText.tsx
+++ pages/components/htmlText.tsx
@@ -1,8 +1,8 @@
-export default function HtmlText(newtest: any, props: any) {
+export default function HtmlText(newtest: any, props: any | null) {
 
     const { author, chapter, paragraph } = props || {};
 
-    const text = <div>{newtest[author][chapter][paragraph]}</div>
+    const text = <div>{newtest?.[author]?.[chapter]?.[paragraph]}</div>
 
     //console.log(text)
     //console.log(author)
